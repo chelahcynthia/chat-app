acd6d66385d93b06f5b8b7760ce798f5f82361d1
chelahcynthia <cchelangat989@gmail.com>
2023-06-12 19:28:50 +0300

    Imports ContextProvider on app.js

--- context/index.js
+++ context/index.js
@@ -4,6 +4,7 @@ export const Context = createContext();
 
 export const ContextProvider = () => {
     const [username, setUsername] = useState(" ");
+    // secret to be used in login. if username and secret is present == user is logged in.
     const [secret, setSecret] = useState(" ");
 
     const value = {
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import "../styles/auth.css"
 import "../styles/chat.css"
 import "../styles/index.css"
 
+import { ContextProvider } from "../context"
+
 
 import React from 'react'
 
 export default function _app({}) {
   return (
-    <div>_app</div>
+   <ContextProvider>
+    
+   </ContextProvider>
   )
 }
